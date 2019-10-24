c5861041c287920746e3b6158fd06c7cbdafb6c2
杨俊艺 <1209032479@qq.com>
2019-10-24 17:31:10 +0800

    对约束作动画完成左右飞出

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,9 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         currentQuestionLabel.text = questions[currentQuestionIndex]
+        updateOffScreenLabel()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -22,15 +22,12 @@ class ViewController: UIViewController {
         nextQuestionLabel.alpha = 0 //下一个问题标签消失
     }
 
-    //当前项目中视图控制器是一个ViewController实例,负责管理Main.storyboard中定义的界面
-    //按住Control从大纲视图中的View Controller拖到试图上方的标签上,当标签高亮时选择questionLabel设置插座变量
-    //当应用载入storyboard时ViewController对象的插座变量就会自动指向位于视图上方的标签对象,通过这个关联,ViewController对象就能在运行时管理这个标签
     @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var answerLabel: UILabel!
     
-    //选中画布中的下一个问题按钮,按住Control并拖至View Controller,当View Controller高亮时选择Sent Events的动作
-    //或者直接将按钮拖动到ViewController.swift代码编辑区生成新的动作
     @IBAction func showNextQuestion(sender: AnyObject)
     {
         currentQuestionIndex += 1
@@ -47,18 +44,30 @@ class ViewController: UIViewController {
         answerLabel.text = answers[currentQuestionIndex]
     }
     
+    func updateOffScreenLabel(){
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
+    }
+    
     //动画
     func animateLabelTransitions(){
+        view.layoutIfNeeded()
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
         UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
             self.currentQuestionLabel.alpha = 0 //当前问题标签消失
             self.nextQuestionLabel.alpha = 1    //下一个问题标签显示
+            self.view.layoutIfNeeded()
         }, completion: {_ in
             swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+            swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+            self.updateOffScreenLabel()
         })
     }
     
     //MARK: 模型对象
-    var currentQuestionIndex: Int = 0                               //跟踪用户正在回答的问题
+    var currentQuestionIndex: Int = 0
     let questions: [String] = ["From what is cognac made?",
                                "What is 7 + 7?",
                                "What is the capital of Vermont?",
